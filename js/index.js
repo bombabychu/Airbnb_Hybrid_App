f17a4794db947f4623ae8dbecafe3608fb56cf11
bombabychu <bombabychu@gmail.com>
2017-10-18 07:11:25 +0900

    hostel list complete

--- js/index.js
+++ js/index.js
@@ -176,6 +176,7 @@ app.member=(()=>{
 app.list=(()=>{
     var onCreate=()=>{
         setContentView();
+
     };
     var setContentView=()=>{
         $('body').empty();
@@ -210,28 +211,116 @@ app.list=(()=>{
             'http://yepan.net/data/file/news/thumb/600x0_90/3531380511_b4cbae22_013.jpg',
             'http://images.contentful.com/22n7d68fswlw/502brzgxXqKCUq0qcQQMiI/b00421bd4d322dd4025228a993e8f178/KIKIDELIVERYSERICE_PHOTO_03.jpg?w=1200&h=630'
         ];
+/*
+        var imgTest =
+            '<div class="main-carousel" data-flickity=\'{ "cellAlign": "left", "contain": true }\'>'+
+            '<div class="carousel" >' +
+            '  <div class="carousel-cell"></div>\n' +
+            '  <div class="carousel-cell"></div>\n' +
+            '  <div class="carousel-cell"></div>\n' +
+            '  <div class="carousel-cell"></div>\n' +
+            '  <div class="carousel-cell"></div>\n' +
+            '</div>'+
+        '</div>';
+        $('#wrapper').empty();
+        $('#wrapper').append(imgTest);*/
 
         $.each(arr,(i,j)=>{
             var content =
-                '<div class="list-content">'+
-                '    <img class="list-img" src="'+arr[i]+'">' +
+                '<div id="content'+i+'" class="list-content">'+
+                '    <img  class="list-img" src="'+arr[i]+'">' +
                 '    <div class="content-txt">' +
                 '        <span id="content-txt-info">개인실 / 침대 1개</span><br>' +
                 '        <span id="content-txt-title">One room in Seoul</span><br>' +
                 '        <span id="content-txt-price">￦16901 /박</span><br>' +
                 '    </div>'
                 '</div>';
-            $('.content').attr('id','content'+i);
             $('#contents').append(content);
         });
-        content += '</div></div></div>';
-        $('#contents').append(content);
 
         $('#container').css({
             'text-align': 'center',
             'margin-top': '5px'
         });
+        $.each(arr,(i,j)=>{
+            $('#content'+i).click(e=>{
+                alert('제발..잠좀자자..' +
+                    ' 선택한 컨텐트 아이디: content'+i);
+                //이 시점에서 디테일로 빠져야한다.
+                app.detail.onCreate();
+            });
+        });
+
     };
+    return {onCreate:onCreate};
+})();
+app.detail=(()=>{
+    var onCreate=()=>{
+        setContentView();
+        $('#back-btn').click(e=>{
+            alert('ㅇ으앙아아아악 졸려!');
+            app.list.onCreate();
+        });
+    };
+    var setContentView=()=>{
+
+        $('body')
+            .html(
+                '<header>'+
+                '  <div >'+
+                '       <span id="back-btn" class="arrow arrow-left"></span>'+
+                '  </div>'+
+                '</header>'
+            )
+            .css({
+            'background-color': '#ffffff',
+            'font-family': '"나눔 고딕","Nanum Gothic","맑은 고딕","Malgun Gothic","Apple Gothic","돋움",Dotum,"Helvetica Neue",Helvetica,Arial,sans-serif !important'
+            });
+        $('header')
+            .css({
+                'position': 'fixed',
+                'width': '100%',
+                'padding':'5%'
+            });
+        $('.arrow')
+            .css({
+                'position': 'relative',
+                'display': 'inline-block',
+                'margin': '8px',
+                'padding': '8px',
+                'box-shadow': '1px -1px 0 0 black inset'
+            });
+        $('.arrow-left').css({
+            'transform': 'rotate(45deg)'
+        });
+        $('<div></div>').attr('id', 'wrapper').appendTo('body');
+        $('#wrapper')
+            .html(
+                '<div id="container">' +
+                    '<div id="contents">' +
+                    '</div>' +
+                '</div>');
+        var detailImg =
+            'http://78.media.tumblr.com/524f336bcedd6025a8ae58bef1a98cd6/tumblr_neevvxrk8V1qk5vl4o1_1280.jpg';
+        $('#contents').before(app.compUI.image('detail-img',detailImg));
+        $('#detail-img').addClass('detail-img');
+        $('#contents').append(
+            '<div class="detail-title">One room in Seoul City</div>\n' +
+            '<div class="detail-content" >개인실<br>호스트: <a href="#">"아이디"</a> 님</div>\n' +
+            '<div class="detail-content-icon" >' +
+            '<div class="hostel-icon"><span class="glyphicon glyphicon-user"></span><br>인원1명</div>\n' +
+            '<div class="hostel-icon"><span class="glyphicon glyphicon-home"></span><br>원룸</div>\n' +
+            '<div class="hostel-icon"><span class="glyphicon glyphicon-bed"></span><br>침대1개</div>\n' +
+            '<div class="hostel-icon"><span class="glyphicon glyphicon-tint"></span><br>욕실1개</div>'+
+            '</div>\n' +
+            '<div class="detail-content" >숙소 소개<br>' +
+            '<div style="font-size: 10px;font-weight: normal;">Room with private bathroom in an old country house, located on a sunny slope overlooking the Idro lake (on the border between Lombardia and Trentino-Alto Adige regions). The house is renewed, finished with pine wood, and with ceramic wood stoves. The room and the bathroom are located in an independent part of the house accessible through a small hallway. There is a spacious kitchen for our guests to use (shared with us). We also have a nice veranda and a very suggestive living room finished with antique wood and sculptures.' +
+            '</div>' +
+            '<div style="border:2px solid greenyellow;margin: 30px;height: 200px;">구글 지도가 들어갈 겁니다.</div>'+
+            '<div style="border: 2px dotted rebeccapurple;width: 150px;text-align: center;margin: 0 auto;">예약 가능 여부 확인</div>'+
+            '</div>');
+
+    };//setContentView Close Brace
     return {onCreate:onCreate};
 })();
 app.cookie={
